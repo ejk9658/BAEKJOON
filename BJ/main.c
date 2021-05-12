c141b6c289b3fe3db36e207cbb7aa169adf63c81
ejk9658 <ejk0729@naver.com>
2021-05-13 00:06:23 +0900

    Initial Commit

---
+++ BJ/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  BJ
+//
+//  Created by 강은진 on 2021/05/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
